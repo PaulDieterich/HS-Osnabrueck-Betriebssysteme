18636e2ac86fd12f552c2f31b432b7a83eeb195d
pjd-vm <Paul.dieterich0@gmail.com>
2020-04-29 22:58:33 +0200

    Aufgabe6

--- Aufgabe6/cmake-build-debug/CMakeFiles/Aufgabe6.dir/cmake_clean.cmake
+++ Aufgabe6/cmake-build-debug/CMakeFiles/Aufgabe6.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
-file(REMOVE_RECURSE
-  "Aufgabe6"
-  "Aufgabe6.pdb"
-  "CMakeFiles/Aufgabe6.dir/main.c.o"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang )
   include(CMakeFiles/Aufgabe6.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
--- Aufgabe6/main.c
+++ Aufgabe6/main.c
@@ -1,27 +1,79 @@
+/**
+ * Betriebssysteme Praktikum Aufgabe6
+ * Threading
+ * Dieterich,Paul und Weimer,Jan
+ *
+ * Todo:
+ *
+  *
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-void* myTurn(void * arg){
-    int *intr = (int*)arg;
-    for(int i = 0; i < 8; i++) {
-        sleep(1);
-        printf("My Turn! %d %d\n",i,*intr);
-        (*intr)++;
+#include <string.h>
+#include <stdlib.h>
+
+//Liste an URL's zum auslesen
+typedef struct URLList{
+    char* url;
+    struct URLList* next;
+} URLList;
+
+int checkIfFileReadabill(char* datei, FILE* file){
+    //modes: "r" = read: Open file for input operations. The file must exit.
+    if(!(file = fopen(datei, "r"))){
+     perror("Steuerdatei konnte nicht geoeffnet werden");
+     return 0;
     }
-    return NULL;
+    return 1;
 }
-void yourTurn(){
-    for(int i = 0; i < 5; i++) {
-        sleep(1);
-        printf("Your Turn! %d\n",i);
+//aus Beispieldatei Web_Request
+void readFile(FILE* file, URLList* urlList, URLList* currList,char url[256]){
+    while(fgets(url, sizeof(url), file)){
+
+        if(strtok(url, "\r\n") == NULL){
+            continue;
+        }
+        if(urlList == NULL){
+            //urlList ist Leer
+            urlList = malloc(sizeof(URLList));
+            currList = urlList;
+        }
+        else{
+            //ein neuer eintrag in der Liste
+            currList->url = malloc(sizeof(URLList));
+            currList->next = NULL;
+        }
+        currList->url = strdup(url);
+        currList->next = NULL;
     }
 }
-int main() {
-    int v = 5;
-    pthread_t newtread;
-    pthread_create(&newtread, NULL,myTurn,&v);
-    yourTurn();
-    pthread_join(&newtread,NULL);
-    printf("thread's done: v=%d", v);
+void* search(void *arg){
+    URLList* list = (URLList*) arg;
+    
+}
+//TODO: Einlesen von Steuerdatei aus den Übergabeparametern
+/*TODO: Ein Thread liest die Zeilen aus der Seuerdatei und schreibt einen entsprechenden Job
+ *      in eine Queue mit einer konfoguierbaren Größe.
+ */
+/*TODO: Eine Konfigurierbare Anzahl von Threads liest
+ * die Jobs aus der Queue und kontakiert die jeweiligen Web-Server
+ */
+
+int main(int argc, char *argv[]) {
+    char* steuerdatei = argv[1];
+    int threadAnzahl = argv[2]; //fuer variable anzahl an threads
+    FILE* file; char url[256];
+    URLList* urlList = NULL;
+    URLList* currList = NULL;
+    pthread_t searchThread1, searchThread2;
+    if(checkIfFileReadabill(steuerdatei,file)){
+        readFile(file,urlList,currList,url);
+        fclose(file); // datei wird geschlossen, sie wurde in checkIfFileReadebill geöffnet
+    }
+
+
+
     return 0;
 }
