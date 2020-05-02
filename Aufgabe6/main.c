7cf55f36f736e914712dff5c8cdb1cc3cd1f19d4
pjd-vm <Paul.dieterich0@gmail.com>
2020-05-02 10:51:40 +0200

    Create ThreadArray

--- Aufgabe6/main.c
+++ Aufgabe6/main.c
@@ -7,12 +7,12 @@
  *
   *
  */
-
 #include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
+
+
 
 //Liste an URL's zum auslesen
 typedef struct URLList{
@@ -20,38 +20,38 @@ typedef struct URLList{
     struct URLList* next;
 } URLList;
 
-int checkIfFileReadabill(char* datei, FILE* file){
-    //modes: "r" = read: Open file for input operations. The file must exit.
-    if(!(file = fopen(datei, "r"))){
-     perror("Steuerdatei konnte nicht geoeffnet werden");
-     return 0;
-    }
-    return 1;
-}
-//aus Beispieldatei Web_Request
-void readFile(FILE* file, URLList* urlList, URLList* currList,char url[256]){
-    while(fgets(url, sizeof(url), file)){
+//GLOBALE VARIABLEN
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+URLList* urlList = NULL;
+URLList* currList = NULL;
 
-        if(strtok(url, "\r\n") == NULL){
-            continue;
-        }
-        if(urlList == NULL){
-            //urlList ist Leer
-            urlList = malloc(sizeof(URLList));
-            currList = urlList;
-        }
-        else{
-            //ein neuer eintrag in der Liste
-            currList->url = malloc(sizeof(URLList));
+//aus Beispieldatei Web_Request
+//FILE* file, URLList* urlList, URLList* currList,char url[256]
+void* readFile(void* arg) {
+    char url[256];
+    FILE* file = (FILE*)arg;
+    if (!(file = fopen(file, "r"))) {
+        perror("Steuerdatei konnte nicht geoeffnet werden");
+    } else {
+        while (fgets(url, sizeof(url),file)) { // erster parameter, wo es reinspeichern soll, zweiter parameter wie lang, dritter parameter die datei.
+            if (strtok(url, "\r\n") == NULL) {
+                continue;
+            }
+            if (urlList == NULL) {
+                //urlList ist Leer
+                urlList = malloc(sizeof(URLList));
+                currList = urlList;
+            } else {
+                //ein neuer eintrag in der Liste
+                currList->url = malloc(sizeof(URLList));
+                currList->next = NULL;
+            }
+            currList->url = strdup(url);
             currList->next = NULL;
         }
-        currList->url = strdup(url);
-        currList->next = NULL;
+        fclose(file);
     }
-}
-void* search(void *arg){
-    URLList* list = (URLList*) arg;
-    
+
 }
 //TODO: Einlesen von Steuerdatei aus den Übergabeparametern
 /*TODO: Ein Thread liest die Zeilen aus der Seuerdatei und schreibt einen entsprechenden Job
@@ -59,20 +59,32 @@ void* search(void *arg){
  */
 /*TODO: Eine Konfigurierbare Anzahl von Threads liest
  * die Jobs aus der Queue und kontakiert die jeweiligen Web-Server
- */
+*/
+void createThreadArray(pthread_t threadArray[]){
+    for(int i = 0; i < sizeof(threadArray); i++){
+        pthread_create(&threadArray[i], NULL,NULL,NULL);
+    }
+}
+void joinThreadArray(pthread_t threadArray[]){
+    for(int i = 0; i < sizeof(threadArray); i++){
+        pthread_join(threadArray[i], NULL);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    char* steuerdatei = argv[1];
-    int threadAnzahl = argv[2]; //fuer variable anzahl an threads
-    FILE* file; char url[256];
-    URLList* urlList = NULL;
-    URLList* currList = NULL;
-    pthread_t searchThread1, searchThread2;
-    if(checkIfFileReadabill(steuerdatei,file)){
-        readFile(file,urlList,currList,url);
-        fclose(file); // datei wird geschlossen, sie wurde in checkIfFileReadebill geöffnet
-    }
+    FILE* file = argv[1];
+    int searchThreadAnzahl = argv[2];
 
+    pthread_t threadArray[searchThreadAnzahl]; // create array of pthread's
+    pthread_t readThread; //create readThread
+
+    createThreadArray(searchThreadAnzahl, threadArray[]); //create vorbestimmte Anzahl an SearchThreads
+    pthread_create(&readThread ,NULL,readFile,file);
+
+
+
+    pthread_join(&readThread, NULL);
+    joinThreadArray(threadArray[],NULL);
 
 
     return 0;
