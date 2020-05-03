143041e7924212eef2cfdb78bea3e7d16fbddae0
pjd-vm <Paul.dieterich0@gmail.com>
2020-05-03 16:05:20 +0200

    pthread_cont_t

--- Aufgabe6/main.c
+++ Aufgabe6/main.c
@@ -7,7 +7,7 @@
 #include <sys/time.h>
 
 #define maxChar 256
-#define QUEUESIZE 30
+#define QUEUESIZE 10
 
 int threadsize = 10;
 char input[maxChar];
@@ -19,6 +19,7 @@ typedef struct {
     long head, tail;
     int full, empty;
     pthread_mutex_t lock;
+    pthread_cond_t *notFull, *notEmpty;
 } queue;
 
 
@@ -180,6 +181,10 @@ void *readFile(void *q) {
             *pos = '\0';
         }
         pthread_mutex_lock(&q2->lock);
+        if (&q2->full) {
+            printf ("QUEUE IS FULL.\n");
+            pthread_cond_wait (&q2->notFull, &q2->lock);
+        }
         queueAdd(q2, url);
         pthread_mutex_unlock(&q2->lock);
         pthread_mutex_destroy(&q2->lock);
@@ -195,11 +200,17 @@ void *writeFile(void *q) {
 
     webreq_init(2, argv);
 
-    pthread_mutex_lock(&tmp->lock);
-    while (!(tmp->empty)) {
+
+    for(int i = 0; i < QUEUESIZE ; i++){
+    //while (!(tmp->empty)) {
+        pthread_mutex_lock(&tmp->lock);
+        if(&tmp->empty){
+            printf ("QUEUE IS EMPTY.\n");
+            pthread_cond_wait (&tmp->notFull, &tmp->lock);
+        }
         char *url = strdup(queueRead(q));
         char *downloadUrl = strdup(url);
-
+        queueDel(tmp,url); //Queue vom eintrag befrien damit wieder platz wird.
         pthread_mutex_unlock(&tmp->lock);
         pthread_mutex_destroy(&tmp->lock);
         strtok(url, "/");
