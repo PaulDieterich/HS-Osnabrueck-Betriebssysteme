21a296d6cbbfc3564379085aec55643fdfb49058
pjd-vm <Paul.dieterich0@gmail.com>
2020-05-08 16:46:58 +0200

    2 unterschiedliche explore funktionen

--- Aufgabe7/main.c
+++ Aufgabe7/main.c
@@ -2,8 +2,11 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdlib.h>
-struct dirent *sd;
-int main(int argc, char* argv[]){
+#include <sys/stat.h>
+#include <string.h>
+
+void explore1(){
+    struct dirent *sd;
     printf("Aufgabe7");
 
     DIR* dir;
@@ -14,10 +17,35 @@ int main(int argc, char* argv[]){
         exit(1);
     }
 
-    while((sd=readdir(dir)) != NULL){
-    printf(" %s\n", sd->d_name);
+    while((sd=readdir(dir)) != NULL) {
+        printf(" %s\n", sd->d_name);
     }
-
     closedir(dir);
+}
+void explore2(){
+    char *dir_name;
+    DIR* dir;
+    struct dirent *entry;
+    struct stat info;
+    dir_name = ".";
+    dir = opendir(dir_name);
+    if(!dir){ printf("Directory not found\n"); exit(1); }
+    while((entry=readdir(dir)) != NULL){
+        if(entry->d_name[0] != "."){
+            char* path;
+            snprintf(path, sizeof path, "%s/%s",dir_name,entry->d_name);
+            printf("Entry = %s", path);
+            stat(path, &info);
+            if(S_ISDIR(info.st_mode)){
+                explore2(path);
+            }
+        }
+    }
+    closedir(dir);
+}
+
+int main(int argc, char* argv[]){
+    explore1();
+    explore2();
     return 0;
 }
