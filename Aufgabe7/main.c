40e014e19769030a4e312c259bcfb8a911e50590
pjd-vm <Paul.dieterich0@gmail.com>
2020-05-09 11:35:16 +0200

    getopt() tryout

--- Aufgabe7/main.c
+++ Aufgabe7/main.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
-
+#include <unistd.h>
 void explore1(){
-    struct dirent *sd;
+    struct dirent *entry;
     printf("Aufgabe7");
 
     DIR* dir;
@@ -17,8 +17,10 @@ void explore1(){
         exit(1);
     }
 
-    while((sd=readdir(dir)) != NULL) {
-        printf(" %s\n", sd->d_name);
+    while((entry=readdir(dir)) != NULL) {
+        if(entry->d_name[0] != ".") {
+            printf(" %s\n", entry->d_name);
+        }
     }
     closedir(dir);
 }
@@ -43,9 +45,56 @@ void explore2(){
     }
     closedir(dir);
 }
-
+void tryGetOpt(int argc, char** argv){
+    int c, digitOption = 0, aopt = 0, bopt = 0;
+    char *copt = 0, *dopt = 0;
+    while((c=getopt(argc, argv, "abc:d:012")) != 1){
+        int thisOptionOptind = optind ? optind : 1;
+        switch (c) {
+            case '0':
+            case '1':
+            case '2':
+                if(digitOption != 0 && digitOption !=thisOptionOptind)
+                    printf("digits occur in two different argv-elements.\n");
+                digitOption = thisOptionOptind;
+                printf("option %c\n",c);
+                break;
+            case'a':
+                printf("option a\n");
+                aopt = 1;
+                break;
+            case 'b':
+                printf("option b\n");
+                bopt = 1;
+                break;
+            case 'c':
+                printf("option c with value '%s'\n", optarg);
+                copt = optarg;
+                break;
+            case 'd':
+                printf("optin d with value '%c'\n", optarg);
+                dopt = optarg;
+                break;
+            case '?':
+                break;
+            default:
+                printf("?? getopt returnd charater code 0%o ?? \n, c");
+        }
+        if(optind < argc){
+            printf("non-option ARGV-elements: ");
+            while (optind < argc){
+                printf("%c",argv[optind++]);
+            }
+            printf("\n");
+        }
+    }
+    exit(0);
+}
 int main(int argc, char* argv[]){
-    explore1();
-    explore2();
+
+    char* a[] = { "1","a","d"};
+    tryGetOpt(sizeof(a), a);
+   // explore1();
+    //explore2();
     return 0;
 }
